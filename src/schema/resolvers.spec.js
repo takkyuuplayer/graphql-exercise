2e4b9ddf0df1321126fc643fc7bff8251490e305
takkyuuplayer <takkyuuplayer@gmail.com>
2017-11-02 20:42:30 -0400

    Add mutation

--- src/schema/resolvers.spec.js
+++ src/schema/resolvers.spec.js
@@ -3,10 +3,30 @@ import assert from 'power-assert';
 import resolvers from './resolvers';
 
 describe('schema/resolvers', () => {
+  const Query = resolvers.Query
   describe('.Query', () => {
-    const Query = resolvers.Query
     it('can return all links', () => {
       assert.strictEqual(Query.allLinks().length, 2);
+    });
+  });
+
+  const Mutation = resolvers.Mutation;
+  describe('.Mutation', () => {
+
+    it('can create new link', () => {
+      Mutation.createLink(null, {
+        url: 'http://www.takkyuuplayer.com',
+        describe: 'my website',
+      });
+
+      const allLinks = Query.allLinks()
+
+      assert.strictEqual(allLinks.length, 3)
+      assert.deepStrictEqual(allLinks[2], {
+        id: 3,
+        url: 'http://www.takkyuuplayer.com',
+        describe: 'my website',
+      });
     });
   });
 });
