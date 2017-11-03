f0ac14bd1c44800e0e0294b2e146dae9e71bb6db
takkyuuplayer <takkyuuplayer@gmail.com>
2017-11-02 20:22:44 -0400

    Add graphiql

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-import { graphqlExpress } from 'apollo-server-express'
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
 import schema from './schema'
 
 const app = express();
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphiql', bodyParser.json(), graphiqlExpress({endpointURL: '/graphql'}));
 
 const PORT = 3000;
 app.listen(PORT, () => {
